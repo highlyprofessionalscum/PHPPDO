fc6a480ae0ceb8ce5f4668ba1ae5dbe7ab2695ef
Litvin <stayeronglass@gmail.com>
2023-03-10 17:38:08 +0300

    222

---
+++ src/PhpPdo.php
@@ -0,0 +1,115 @@
+<?php
+
+declare(strict_types=1);
+namespace PhpPdo;
+
+use PhpPdo\Driver\DriverInterface;
+use PhpPdo\PdoInterface as PhpPdoInterface;
+use PhpPdo\Driver\OdbcDriver;
+
+class PhpPdo implements PhpPdoInterface
+{
+
+    private DriverInterface $driver;
+    private int $errorMode = PdoInterface::ERRMODE_SILENT;
+
+    public function __construct(string $dsn,
+                                ?string $username = null,
+                                ?string $password = null,
+                                ?array $options = null)
+    {
+        $this->driver = new OdbcDriver($dsn);
+    }
+
+    public function beginTransaction(): bool
+    {
+        // TODO: Implement beginTransaction() method.
+    }
+
+    public function commit(): bool
+    {
+        // TODO: Implement commit() method.
+    }
+
+    public function errorCode(): ?string
+    {
+        // TODO: Implement errorCode() method.
+    }
+
+    public function errorInfo(): array
+    {
+        // TODO: Implement errorInfo() method.
+    }
+
+    public function exec(string $statement)
+    {
+        return $this->driver->exec($statement);
+    }
+
+    public function getAttribute(int $attribute): mixed
+    {
+        // TODO: Implement getAttribute() method.
+    }
+
+    public static function getAvailableDrivers(): array
+    {
+        // TODO: Implement getAvailableDrivers() method.
+    }
+
+    public function inTransaction(): bool
+    {
+        // TODO: Implement inTransaction() method.
+    }
+
+    public function lastInsertId(?string $name = null)
+    {
+        // TODO: Implement lastInsertId() method.
+    }
+
+    /**
+     * @param string $query
+     * @param array $options
+     * @return false|PDOStatement
+     * @throws \ReflectionException
+     */
+    public function prepare(string $query, array $options = [])
+    {
+        if (false === $statement = $this->driver->prepare($query)) return false;
+
+        $reflectionClass = new \ReflectionClass('PhpPdo\PDOStatement');
+
+        $result = $reflectionClass->newInstanceWithoutConstructor();
+
+        $reflectionProperty = $reflectionClass->getProperty('driver');
+        $reflectionProperty->setAccessible(true);
+        $reflectionProperty->setValue($result, $this->driver);
+        $reflectionProperty->setAccessible(false);
+
+        $reflectionProperty = $reflectionClass->getProperty('handle');
+        $reflectionProperty->setAccessible(true);
+        $reflectionProperty->setValue($result, $statement);
+        $reflectionProperty->setAccessible(false);
+
+        return $result;
+    }
+
+    public function query(string $query, ?int $fetchMode = null)
+    {
+        // TODO: Implement query() method.
+    }
+
+    public function quote(string $string, int $type = PhpPdo::PARAM_STR)
+    {
+        // TODO: Implement quote() method.
+    }
+
+    public function rollBack(): bool
+    {
+        // TODO: Implement rollBack() method.
+    }
+
+    public function setAttribute(int $attribute, mixed $value): bool
+    {
+        // TODO: Implement setAttribute() method.
+    }
+}
---
+++ src/Test/Php/pdo_003Test.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace PhpPdo\Test\Php;
+
+use PhpPdo\PdoInterface;
+use PHPUnit\Framework\TestCase;
+
+use PhpPdo\PhpPdo;
+
+final class pdo_003Test extends TestCase
+{
+    public function setUp(): void
+    {
+        $db = new PhpPdo('pdo3');
+        $test_tables = array(
+            'test',
+            'test2',
+            'classtypes'
+        );
+        foreach ($test_tables as $table) {
+            $db->exec("DROP TABLE $table");
+        }
+    }
+
+    public function test_FETCH_NUM(): void
+    {
+        $db = new PhpPdo('pdo3');
+        $db->exec('CREATE TABLE test(id int NOT NULL PRIMARY KEY, val VARCHAR(10))');
+        $db->exec("INSERT INTO test VALUES(1, 'A')");
+        $db->exec("INSERT INTO test VALUES(2, 'B')");
+        $db->exec("INSERT INTO test VALUES(3, 'C')");
+
+        $stmt = $db->prepare('SELECT * from test');
+        $stmt->execute();
+
+        $res = $stmt->fetchAll(PhpPdo::FETCH_BOTH);
+
+        $this->assertEquals([
+            0 => ['0' => '1', 'id' => '1', '1' => 'A', 'val' => 'A',],
+            1 => ['0' => '2', 'id' => '2', '1' => 'B', 'val' => 'B'],
+            2 => ['0' => '3', 'id' => '3', '1' => 'C', 'val' => 'C'],
+        ], $res);
+
+    }
+}
